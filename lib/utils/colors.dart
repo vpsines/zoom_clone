b47681ed99f440cb5902510f35d866e84050e268
vpsines <sasip6028@gmail.com>
2023-02-11 19:01:42 +0530

    style: add app colors

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,3 +1,6 @@
 import 'package:flutter/material.dart';
 
-const Color backgroundColor = Colors.black;
+const Color backgroundColor = Color.fromRGBO(36, 36, 36, 1);
+const Color buttonColor = Color.fromRGBO(14, 114, 236, 1);
+const Color footerColor = Color.fromRGBO(26, 26, 26, 1);
+const Color secondaryBackgroundColor = Color.fromRGBO(46, 46, 46, 1);
