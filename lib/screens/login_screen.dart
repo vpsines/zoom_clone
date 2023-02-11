c2f8e9b2e0ba980e437f5f2bb13bc524e88dbd70
vpsines <sasip6028@gmail.com>
2023-02-11 20:19:46 +0530

    feat: add login screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:zoom_clone/screens/home_screen.dart';
+import 'package:zoom_clone/services/auth_services.dart';
+import 'package:zoom_clone/utils/images.dart';
+import 'package:zoom_clone/widgets/custom_button.dart';
 
 class LoginScreen extends StatefulWidget {
   static const String routeName = '/login';
@@ -9,17 +13,32 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+
+  AuthServices authServices = AuthServices();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text('Start or Join meeting',
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold
-          ),)
+          const Text(
+            'Start or Join meeting',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 38),
+            child: Image.asset(
+              onboarding,
+            ),
+          ),
+          CustomButton(onTap: () async{
+            bool result = await authServices.signInWithGoogle(context);
+            if(!mounted) return;
+            if(result  ){
+              Navigator.pushNamed(context, HomeScreen.routeName);
+            }
+          }, buttonText: "Login")
         ],
       ),
     );
